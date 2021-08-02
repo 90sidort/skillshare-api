cabdef2857b3de5eaf7bda5063030df96573400c
andrew <90sidort@gmail.com>
2021-08-03 00:06:16 +0200

    add deapply tests

--- test/actions/actions.spec.ts
+++ test/actions/actions.spec.ts
@@ -59,84 +59,95 @@ describe('E2E actions tests', () => {
   afterAll(async () => {
     await app.close();
   });
-  // it('Should be able to apply for an offer', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${userToken}`)
-  //     .send({ offerId: 11110 });
-  //   expect(result.status).toEqual(200);
-  //   expect(result.body).toMatchObject({
-  //     id: 11110,
-  //     title: 'Seamless',
-  //     description:
-  //       'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
-  //     available: true,
-  //     limit: 8,
-  //     ownerId: 11110,
-  //     skillId: 11110,
-  //     status: 1,
-  //     participants: [],
-  //     applicants: [
-  //       {
-  //         id: 111100,
-  //         username: 'kduberry2r',
-  //         name: 'Kyle',
-  //         surname: 'Duberry',
-  //         email: 'kduberry2r@opensource.org',
-  //         about:
-  //           'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
-  //         roles: 'user',
-  //         applied: [],
-  //         participates: [],
-  //       },
-  //     ],
-  //   });
-  // });
-  // it('Should not be able to apply for own offer', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${userToken}`)
-  //     .send({ offerId: 111101 });
-  //   expect(result.status).toEqual(400);
-  //   expect(result.body.message).toEqual("Can't apply for your own offer");
-  // });
-  // it('Should not be able to apply for nonexisting offer', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${userToken}`)
-  //     .send({ offerId: 997 });
-  //   expect(result.status).toEqual(404);
-  //   expect(result.body.message).toEqual('Offer with id 997 not found!');
-  // });
-  // it('Should not be able to apply if offer limit reached', async () => {
-  //   await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .send({ offerId: 11119 });
-  //   const result = await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${userToken}`)
-  //     .send({ offerId: 11119 });
-  //   expect(result.status).toEqual(404);
-  //   expect(result.body.message).toEqual(
-  //     'Offer with id 11119 has already reached skill share limit of 1!',
-  //   );
-  // });
-  // it('Should not be able to apply if already applied/ participates in more than 10 offers', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .patch('/actions/apply')
-  //     .set('Authorization', `Bearer ${limitToken}`)
-  //     .send({ offerId: 11130 });
-  //   expect(result.status).toEqual(404);
-  //   expect(result.body.message).toEqual(
-  //     'User with id 11144 has already reached skill share limit of 10!',
-  //   );
-  // });
+  it('Should be able to apply for an offer', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ offerId: 11110 });
+    expect(result.status).toEqual(200);
+    expect(result.body).toMatchObject({
+      id: 11110,
+      title: 'Seamless',
+      description:
+        'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+      available: true,
+      limit: 8,
+      ownerId: 11110,
+      skillId: 11110,
+      status: 1,
+      participants: [],
+      applicants: [
+        {
+          id: 111100,
+          username: 'kduberry2r',
+          name: 'Kyle',
+          surname: 'Duberry',
+          email: 'kduberry2r@opensource.org',
+          about:
+            'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+          roles: 'user',
+          applied: expect.any(Array),
+          participates: [],
+        },
+      ],
+    });
+  });
+  it('Should not be able to apply for own offer', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ offerId: 111101 });
+    expect(result.status).toEqual(400);
+    expect(result.body.message).toEqual("Can't apply for your own offer");
+  });
+  it('Should not be able to apply for nonexisting offer', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ offerId: 997 });
+    expect(result.status).toEqual(404);
+    expect(result.body.message).toEqual('Offer with id 997 not found!');
+  });
+  it('Should not be able to apply if offer limit reached', async () => {
+    await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ offerId: 11119 });
+    const result = await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ offerId: 11119 });
+    expect(result.status).toEqual(404);
+    expect(result.body.message).toEqual(
+      'Offer with id 11119 has already reached skill share limit of 1!',
+    );
+  });
+  it('Should not be able to apply if already applied/ participates in more than 10 offers', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/actions/apply')
+      .set('Authorization', `Bearer ${limitToken}`)
+      .send({ offerId: 11130 });
+    expect(result.status).toEqual(404);
+    expect(result.body.message).toEqual(
+      'User with id 11144 has already reached skill share limit of 10!',
+    );
+  });
   it('Should be able to resing from application', async () => {
     const result = await request(app.getHttpServer())
       .patch('/actions/deapply')
       .set('Authorization', `Bearer ${userToken}`)
       .send({ offerId: 11123 });
-    console.log(result.body);
+    expect(result.status).toEqual(200);
+    result.body.applicants.forEach((applicant) => {
+      if (applicant.id === 111100) expect(false).toBe(true);
+    });
+  });
+  it('Should be able to return error if resing offer from application dsnt exist', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/actions/deapply')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ offerId: 997 });
+    expect(result.status).toEqual(404);
+    expect(result.body.message).toEqual('Offer with id 997 not found!');
   });
 });
